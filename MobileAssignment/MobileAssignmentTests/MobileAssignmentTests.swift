69c7edde4ee24d9aec768b0be9c224ede3297db0
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2025-08-28 16:30:03 +0530

    init

---
+++ MobileAssignment/MobileAssignmentTests/MobileAssignmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  MobileAssignmentTests.swift
+//  MobileAssignmentTests
+//
+//  Created by KavinduDissanayake on 2025-08-28.
+//
+
+import Testing
+
+struct MobileAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
